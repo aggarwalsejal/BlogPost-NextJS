7aacbdc8e35d147464aa52890b48545fd0d87775
Sejal Aggarwal <sejalaggarwal0@gmail.com>
2023-08-20 16:43:41 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,14 +22,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className={styles.mainnav}>
-        <ul>
-          <Link href='/'><li>Home</li></Link>
-          <Link href='/about'><li>About</li></Link>
-          <Link href='/blog'><li>Blog</li></Link>
-          <Link href='/contact'><li>Contact</li></Link>
-        </ul>
-      </nav>
+     
       <main className={styles.main}>
       <h1 className={styles.title}>
         Blog Post
