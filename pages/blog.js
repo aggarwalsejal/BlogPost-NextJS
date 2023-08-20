751c616fc5b1862de2fd8d74e083b69d5dab72e4
Sejal Aggarwal <sejalaggarwal0@gmail.com>
2023-08-20 18:44:40 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import styles from "@/styles/Blog.module.css";
+import Link from 'next/link';
 
 const Blog = () => {
   return (
-    <div>
-      This is blog page
+    <div className={styles.conatainer}>
+      <main className={styles.main}>
+      <h2>Popular Blogs</h2>
+    <div className={styles.blogs}>
+     <Link href={'/blogpost/learn-javascript'}>
+      <h3>first blog</h3></Link>
+      <p>Series method to discuss small problems</p>
+    </div>
+    <div className={styles.blogs}>
+      <h3>first blog</h3>
+      <p>Series method to discuss small problems</p>
+    </div>
+    <div className={styles.blogs}>
+      <h3>first blog</h3>
+      <p>Series method to discuss small problems</p>
+    </div>
+    <div className={styles.blogs}>
+      <h3>first blog</h3>
+      <p>Series method to discuss small problems</p>
+    </div>
+    </main>
     </div>
   )
 }
