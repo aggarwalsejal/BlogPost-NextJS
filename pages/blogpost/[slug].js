180ca0acfa1c43a1f7353c6c710524a166b73142
Sejal Aggarwal <sejalaggarwal0@gmail.com>
2023-08-20 18:43:51 +0530

    Update [slug].js

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,20 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "@/styles/BlogPost.module.css";
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug} </div>;
+  return   <div className={styles.conatainer}>
+  <main className={styles.main}>
+    <h1>Title of the page {slug}</h1>
+    <hr />
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex natus ipsam voluptas consequuntur architecto perspiciatis magnam dolorem, praesentium obcaecati quaerat aliquam eius accusantium!
+    </div>
+    </main>
+    </div>;
 };
 
 export default slug;
+
